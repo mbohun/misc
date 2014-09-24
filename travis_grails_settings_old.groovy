ba5f9ecdf5ef08da4b6c9f7e3b6042e668ebe5fd
mbohun <martin.bohun@gmail.com>
2014-09-24 17:10:22 +1000

    working version/format for older grails projects, this was tested with dashboard that uses grails 2.2.4

--- travis_grails_settings_old.groovy
+++ travis_grails_settings_old.groovy
@@ -1,15 +1,10 @@
+// this file format was/is required for older grails apps, like dashboard (used to figure this out and test) that uses grails 2.2.4
 grails.project.dependency.distribution = {
-  remoteRepository(id:"nectar-nexus-repo-snapshot",
-		   url:"http://130.56.249.242/nexus/content/repositories/snapshots")
+  remoteRepository(id:"nectar-nexus-repo-snapshot", url:"http://130.56.249.242/nexus/content/repositories/snapshots") {
+    authentication username: System.getenv("TRAVIS_DEPLOY_USERNAME"), password: System.getenv("TRAVIS_DEPLOY_PASSWORD")
+  }
 
-  remoteRepository(id:"nectar-nexus-repo-release",
-		   url:"http://130.56.249.242/nexus/content/repositories/releases")
+  remoteRepository(id:"nectar-nexus-repo-release",  url:"http://130.56.249.242/nexus/content/repositories/releases") {
+    authentication username: System.getenv("TRAVIS_DEPLOY_USERNAME"), password: System.getenv("TRAVIS_DEPLOY_PASSWORD")
+  }
 }
-
-//grails.project.repos.'nectar-nexus-repo-snapshot'.url = "http://130.56.249.242/nexus/content/repositories/snapshots/"
-grails.project.repos.'nectar-nexus-repo-snapshot'.username = System.getenv("TRAVIS_DEPLOY_USERNAME")
-grails.project.repos.'nectar-nexus-repo-snapshot'.password = System.getenv("TRAVIS_DEPLOY_PASSWORD")
-
-//grails.project.repos.'nectar-nexus-repo-release'.url = "http://130.56.249.242/nexus/content/repositories/releases/"
-grails.project.repos.'nectar-nexus-repo-release'.username = System.getenv("TRAVIS_DEPLOY_USERNAME")
-grails.project.repos.'nectar-nexus-repo-release'.password = System.getenv("TRAVIS_DEPLOY_PASSWORD")
